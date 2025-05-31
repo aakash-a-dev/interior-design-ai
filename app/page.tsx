b9b53ba42f2dd3d8a786678c4b165f405f5b2846
Aakash Amod Rajput <aakash.ar1213@gmail.com>
2025-05-31 11:41:13 +0530

    More

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,6 @@ export default function HomePage() {
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20 background-gradient">
         <a
-          href="https://vercel.fyi/roomGPT"
-          target="_blank"
-          rel="noreferrer"
           className="border border-gray-700 rounded-lg py-2 px-4 text-gray-400 text-sm mb-5 transition duration-300 ease-in-out"
         >
          Design your room{" "}
